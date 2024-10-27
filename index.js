71fe5be9ce4c23b02fc10c33401b71c81107c12c
Дмитро Недєв <dmytro.nedyev@o365.opu.ua>
2024-10-27 13:07:32 +0300

    reviews slider section

--- index.js
+++ index.js
@@ -24,3 +24,56 @@ inputs.concat(textareas).forEach(input => {
     }
   })
 });
+
+
+// Slider
+
+let swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 5000
+  },
+  slidesPerView: 'auto',
+  loop: true,
+  loopAddBlankSlides: true,
+  // breakpoints: {
+  //   // when window width is >= 320px
+  //   320: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20
+  //   },
+  //   // when window width is >= 480px
+  //   480: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30
+  //   },
+  //   // when window width is >= 640px
+  //   768: {
+  //     slidesPerView: 6,
+  //     spaceBetween: 20,
+  //     spaceBetweenRows: 50
+  //   }
+  // },
+  grid: {
+    rows: 2,
+    fill: 'row'
+  },
+  spaceBetween: 20,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+});
+
+
+// Відгуки
+
+let starsWrappers = document.querySelectorAll('.stars-wrapper');
+
+starsWrappers.forEach(starsWrapper => {
+  let starsCount = starsWrapper.classList[1][6];
+  let children = starsWrapper.children;
+
+  for (let i = 0; i < starsCount; i++) {
+    children[i].classList.add('checked');
+  }
+});
